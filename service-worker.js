11ca9f6c48c2cc4d3ac1089919955f3ea85f1a97
iskprod <155991809+iskprod@users.noreply.github.com>
2025-08-08 10:07:25 +0100

    add filles to app
    Add more functionality to the app

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'kraou-app-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
